e2083aae4b52f4be9e07f65d27074bb4e1f2d33b
Diep The Sang <110006283+diepthesang@users.noreply.github.com>
2023-06-12 03:23:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // init app
-// tin nang 1.1
-// tinh nang 2
-// tinh nang 3.1
-// tinh nang 4.1
-// tinh nang 5.1
+// tinh nang 1.2
+// tinh nang 2.2
+// tinh nang 3.2
+// tinh nang 4.2
+// tinh nang 5.2
